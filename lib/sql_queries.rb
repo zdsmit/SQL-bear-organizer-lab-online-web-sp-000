649f4eebac3416018c6a0959b0717a18ba0006d6
Zach  Smith <zdsmith001@gmail.com>
2019-10-07 13:51:56 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,21 +11,21 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears WHERE age = (SELECT MAX(age) from bears);"
+  "SELECT name, age FROM bears WHERE name = 'Mr. Chocolate' AND age = (SELECT MAX(age) from bears);"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE age = (SELECT MIN(age) from bears);"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color, COUNT(color) from bears WHERE color = (SELECT MAX(color) from bears);"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(temperament) from bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT id, name, age, gender, color, temperament, alive FROM bears WHERE id = 8;"
 end
